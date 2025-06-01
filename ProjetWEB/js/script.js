47b9425b14a9e8ed5d167cf201a8dff12e3459ce
Iestyn-Jones <iestyn.jones@efrei.net>
2025-06-01 12:34:47 +0200

    Add files via upload

---
+++ ProjetWEB/js/script.js
@@ -0,0 +1,19 @@
+function allerjeu(){
+    document.location.href ="game.html";
+}
+
+function allerdata(){
+    document.location.href = "data.html";
+}
+
+function allernouveaute(){
+    document.location.href = "nouveaute.html";
+}
+
+function allerproblematique(){
+    document.location.href = "problematique.html";
+}
+
+function allerapropos(){
+    document.location.href = "apropos.html";
+}
